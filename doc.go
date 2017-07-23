e077b51fd92f7382e015b0e1bee1756b6e9bf284
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-07-23 12:31:59 +0300

    Godoc the package

---
+++ doc.go
@@ -0,0 +1,32 @@
+// Package sg provides SendGrid APIv3 client for sending transactional mails.
+//
+// The simplest to send a mail is through `sg.Send` which uses a global client:
+//
+//	func main() {
+//		// If you have your key in the SG_API_KEY environment variable, you may
+//		// skip this step.
+//		sg.Setup(sg.NewClient("API_KEY"))
+//
+//		sg.Send(&sg.Mail{
+//			TemplateId:    "c2723c5e-b693-4086-968f-bd9057cc6ae4",
+//			From:          "from@example.com",
+//			To:            "to@example.com",
+//			Substitutions: sg.H{"SUB": "value"},
+//		})
+//	}
+//
+// If you don't want to use a global client, you can build your own:
+//
+//	func main() {
+//		// If you have your key in the SG_API_KEY environment variable, you may
+//		// skip this step.
+//		client := sg.NewClient("API_KEY")
+//
+//		client.Send(&sg.Mail{
+//			TemplateId:    "c2723c5e-b693-4086-968f-bd9057cc6ae4",
+//			From:          "from@example.com",
+//			To:            "to@example.com",
+//			Substitutions: sg.H{"SUB": "value"},
+//		})
+//	}
+package sg
